e005f9aef8c5c51bc3e3da867d8d6a50b5f50d28
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-12-19 00:50:06 +0200

    Coin flip command added

---
+++ commands/coinflip.ts
@@ -0,0 +1,17 @@
+import type { ChatInputCommandInteraction } from "discord.js";
+import type { CommandOptions } from "../types";
+
+export const description =
+	"Flips a coin and replies with either heads or tails";
+export const options: CommandOptions[] = [];
+
+export const run = (interaction: ChatInputCommandInteraction) => {
+	var chance = Math.floor(Math.random() * 2);
+	let result = "Tails!";
+
+	if (chance === 0) {
+		result = "Heads!";
+	}
+
+	interaction.reply({ content: `Your coin landed on ${result}` });
+};
