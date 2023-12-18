bef3d66919052f413da495cbb4635c13cd15cb92
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-12-18 21:21:20 +0200

    Added command options to command template

--- commands/ping.ts
+++ commands/ping.ts
@@ -1,6 +1,8 @@
 import type { ChatInputCommandInteraction } from "discord.js";
+import type { CommandOptions } from "../types";
 
 export const description = "Replies with Pong!";
+export const options: CommandOptions[] = [];
 
 export const run = async (interaction: ChatInputCommandInteraction) => {
 	await interaction.reply({ content: "Pong!", ephemeral: true });
