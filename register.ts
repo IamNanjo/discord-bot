96d7797d8c0926da7583c414be70f8a997bb0a16
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-12-18 20:56:20 +0200

    Slash command registration added

---
+++ register.ts
@@ -0,0 +1,34 @@
+import { REST, Routes } from "discord.js";
+import type { RunnableCommands } from "./imports";
+
+interface Command {
+	name: string;
+	description: string;
+}
+
+export default async (token: string, runnableCommands: RunnableCommands) => {
+	const commandNames = Object.keys(runnableCommands);
+
+	let commands: Command[] = [];
+
+	for (let i = 0, len = commandNames.length; i < len; i++) {
+		const commandName = commandNames[i];
+		commands.push({
+			name: commandName,
+			description: runnableCommands[commandName].description
+		});
+	}
+
+	const rest = new REST({ version: "10" }).setToken(token);
+
+	const appId = ((await rest.get(Routes.currentApplication())) as any).id;
+
+	await rest
+		.put(Routes.applicationCommands(appId), { body: commands })
+		.then(() => {
+			console.log(
+				"\x1b[32mSuccesfully reloaded application slash commands\x1b[0m"
+			);
+		})
+		.catch((err) => console.error(`\x1b[31m${err}\x1b[0m`));
+};
