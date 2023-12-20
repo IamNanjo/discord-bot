7163dbe9481b716988df6db52c0943cebef4c6e3
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-12-20 02:39:54 +0200

    SQLite 3 db added using Prisma

--- index.ts
+++ index.ts
@@ -2,25 +2,39 @@ import { Client, GatewayIntentBits } from "discord.js";
 
 import getToken from "./token";
 import registerCommands from "./register";
+import db from "./db";
 import { importCommands, importEvents } from "./imports";
 
 const token = getToken();
 const commands = await importCommands();
 const events = await importEvents();
-await registerCommands(token, commands);
+if (process.argv.at(-1) === "--register")
+	await registerCommands(token, commands);
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on("ready", () => {
-	console.log(`\n${client.user?.displayName} is ready\n`);
+	console.log(`${client.user?.displayName} is ready\n`);
+});
+
+// Add and remove guilds from db on join and leave
+client.on("guildCreate", async (guild) => {
+	await db.chat.upsert({
+		where: { id: guild.id },
+		create: { id: guild.id },
+		update: {}
+	});
+});
+client.on("guildDelete", async (guild) => {
+	await db.chat.delete({ where: { id: guild.id } });
 });
 
 client.on("interactionCreate", (interaction) => {
 	if ("customId" in interaction) {
-		const eventDetails = interaction.customId.split("_");
+		const eventDetails = interaction.customId.split(":");
 		const eventName = eventDetails[0];
 		const eventArgs = eventDetails.slice(1);
-		const event = events.eventName;
+		const event = events[eventName];
 
 		if (eventName in events)
 			return eventArgs.length
