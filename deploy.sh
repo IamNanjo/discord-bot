a47d8df8e754564589fe186eca10a574355f1bc6
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-01-17 23:50:30 +0200

    Flush logs before restart

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 builtin source ~/.env
 
-~/.local/bin/mise x bun -- bun install
+~/.local/bin/mise x bun -- pm2 flush &&
+~/.local/bin/mise x bun -- bun install &&
 ~/.local/bin/mise x bun -- bun run start
